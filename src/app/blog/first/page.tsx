541a68393c6fbe13d5d9ac8c40152a67f32017f7
shafkat63 <msm.shafkat99@gmail.com>
2024-01-30 13:49:54 +0600

    Nested Route

---
+++ src/app/blog/first/page.tsx
@@ -0,0 +1,5 @@
+const First = () => {
+	return <h1>First Blog Post</h1>;
+};
+
+export default First;
---
+++ src/app/blog/second/page.tsx
@@ -0,0 +1,5 @@
+const Second = () => {
+	return <h1>Second Blog Post</h1>;
+};
+
+export default Second;
