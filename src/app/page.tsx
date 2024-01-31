0c46626760c74214aacdd4cd2a3f9f27733ba043
shafkat63 <msm.shafkat99@gmail.com>
2024-02-01 02:08:02 +0600

    Link component navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,35 @@
+import Link from "next/link";
+
 const Home = () => {
-	return <h1 className="">Home Page</h1>;
+	return (
+		<div className="">
+			<h1>Home page</h1>
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "column",
+					justifyContent: "center",
+					alignItems: "center",
+					color: "green",
+					paddingBottom: "1rem",
+					margin: "1rem",
+				}}
+			>
+				{" "}
+				<Link
+					href="/blog"
+					
+				>
+					Blog
+				</Link>
+				<Link href="/products">Products</Link>
+				<Link href="/about">About</Link>
+				<Link style={{
+						color: "green",
+					}} href="/dashboard">Dashboard</Link>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,53 @@
+import Link from "next/link";
+
 const Products = () => {
+	const productId = 100;
 	return (
 		<div className="flex flex-col justify-center	items-center">
 			<h1>Product list</h1>
-			<h3>Product 1</h3>
-			<h3>Product 2</h3>
-			<h3 className="text-blue-500">Product 3</h3>
-			<h3>Product 4</h3>
-			<h3 className="">Product 5</h3>
+			<h3>
+				{" "}
+				<Link href="/products/1">Product 1</Link>{" "}
+			</h3>
+			<h3>
+				{" "}
+				<Link href="/products/2">Product 2</Link>{" "}
+			</h3>
+			<h3>
+				{" "}
+				<Link href="/products/3">Product 3</Link>{" "}
+			</h3>
+			<h3>
+				{" "}
+				<Link href="/products/4">Product 4</Link>{" "}
+			</h3>
+			<h3>
+				{" "}
+				<Link href="/products/5">Product 5</Link>{" "}
+			</h3>
+			<h3>
+				{" "}
+				<Link href="/products/6">Product 6</Link>{" "}
+			</h3>
+			<h3>
+				{" "}
+				<Link href="/products/7">Product 7</Link>{" "}
+			</h3>
+			<h3>
+				{" "}
+				<Link href="/products/8">Product 8</Link>{" "}
+			</h3>
+		
+			<h3>
+				{" "}
+				<Link href="/products/9" replace>
+					Product 9 || It will clear the link
+				</Link>{" "}
+			</h3>
+			<h3>
+				{" "}
+				<Link href={`/products/${productId}`}>Product {productId} </Link>{" "}
+			</h3>
 		</div>
 	);
 };
