d6690d398f905900a78cd71af50252c45150ff7e
shafkat63 <msm.shafkat99@gmail.com>
2024-02-02 00:33:12 +0600

    Loading state

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,7 +5,16 @@ export const metadata: Metadata = {
 	},
 };
 const Blog = () => {
-	return <h2>My Blog</h2>;
+	let sum = 0;
+	for (let i = 1000000000; i >= 1; i--) {
+		sum = sum + i;
+	}
+
+	return (
+		<div>
+			<h2>My Blog</h2>
+		</div>
+	);
 };
 
 export default Blog;
