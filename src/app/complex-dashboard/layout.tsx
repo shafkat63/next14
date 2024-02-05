41618816dabcb7b4c34ad93e9dd50ad38b8543ef
shafkat63 <msm.shafkat99@gmail.com>
2024-02-06 01:03:51 +0600

    Conditional Routes

--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -8,13 +8,16 @@ const DashboardLayout = ({
 	users,
 	revenue,
 	notifications,
+	login,
 }: {
 	children: React.ReactNode;
 	users: React.ReactNode;
 	revenue: React.ReactNode;
 	notifications: React.ReactNode;
+	login: React.ReactNode;
 }) => {
-	return (
+	const isLoggedIn = false;
+	return isLoggedIn? (
 		<div>
 			<div>{children} </div>
 			<div style={{ display: "flex" }}>
@@ -25,7 +28,9 @@ const DashboardLayout = ({
 				<div style={{ display: "flex", flex: 1 }}>{notifications} </div>
 			</div>
 		</div>
-	);
+	):(
+		login
+	)
 };
 
 export default DashboardLayout;
