257ca916ffa36b78ae98671583831bd7c63b73f7
shafkat63 <msm.shafkat99@gmail.com>
2024-02-09 00:51:52 +0600

    Handling POST Request

--- src/app/comments/data.ts
+++ src/app/comments/data.ts
@@ -1,4 +1,4 @@
-export const comment = [
+export const comments = [
 	{
 		id: 1,
 		text: "This is the first comment",
--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,4 +1,19 @@
-import { comment } from "./data";
+import { comments } from "./data";
 export async function GET() {
-	return  Response.json(comment);
+	return Response.json(comments);
+}
+
+export async function POST(request: Request) {
+	const comment = await request.json();
+	const newComment = {
+		id: comments.length + 1,
+		text: comment.text,
+	};
+	comments.push(newComment);
+	return new Response(JSON.stringify(newComment), {
+		headers: {
+			"Content-Type": "application/json",
+		},
+		status: 201,
+	});
 }
