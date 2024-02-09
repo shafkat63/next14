8f8e273a34050a1dfe65a1626defbbf0961a687b
shafkat63 <msm.shafkat99@gmail.com>
2024-02-10 01:15:36 +0600

    Cookies in Route Handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -8,16 +8,20 @@
 // }
 
 import { type NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 export async function GET(request: NextRequest) {
 	const requestHeaders = new Headers(request.headers);
+	const theme = request.cookies.get("theme");
+	cookies().set("resultsPerPage", "20");
 	const headerList = headers();
 	console.log(requestHeaders.get("Authorization"));
 	console.log(headerList.get("Authorization"));
+	console.log(cookies().get("resultsPerPage"));
 
 	return new Response("<h1>Profile API data</h1>", {
 		headers: {
 			"Content-Type": "text/html",
+			"Set-Cookie": "theme=dark",
 		},
 	});
 }
